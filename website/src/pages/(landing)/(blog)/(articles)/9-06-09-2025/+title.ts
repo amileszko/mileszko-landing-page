22a0dd74aaa2d3ac04f032516cc1ad86c1888f48
amileszko <amadeusz@mileszko.pl>
2025-09-06 09:58:16 +0200

    Add new blog article.

---
+++ website/src/pages/(landing)/(blog)/(articles)/9-06-09-2025/+title.ts
@@ -0,0 +1,9 @@
+import { type PageContext } from "vike/types";
+
+import i18next from "../../../../../i18n/config";
+
+export default (pageContext: PageContext) =>
+  i18next.t(
+    "articles:articles.9-06-09-2025.title",
+    { lng: pageContext.locale },
+  );
